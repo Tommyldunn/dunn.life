734888fe13be9c02b0fb062831615e35971060a5
tommyldunn <tommylogandunn@gmail.com>
2017-12-05 09:01:38 -0500

    init

---
+++ config/webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const webpack = require('webpack');
+const sharedPlugins = require('./sharedPlugins');
+
+const plugins = [
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify('development')})
+];
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    path.join(__dirname, '/../app/src/main.js')
+  ],
+  output: {
+    path: path.join(__dirname, '/../dist/'),
+    filename: '[name].bundle.js',
+    publicPath: '/'
+  },
+  plugins: plugins.concat(sharedPlugins),
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.jsx$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['latest']
+            }
+          }
+        ]
+      }, {
+        test: /\.scss/,
+        use: [
+          'style-loader', {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          }, {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              includePaths: [path.join(__dirname, '/../app/src/css/')]
+            }
+          }
+        ]
+      }, {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8000, // Convert images < 8kb to base64 strings
+              name: 'images/[hash]-[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
